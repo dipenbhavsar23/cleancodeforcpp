5c0330281501bd87cc5f035bd04afb10aa1ca6fb
dipenbhavsar23 <dipenbhavsar.23@gmail.com>
2020-07-20 15:47:38 +0530

    Reducing CCN

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,7 +1,15 @@
-
+bool bpmcheck(float bpm){ 
+  return(bpm < 70 || bpm > 150);
+}
+bool spo2check(float spo2){
+return (spo2 < 80);
+}
+bool respRatecheck(float respRatecheck){
+return (respRate < 30 || respRate > 60);
+}
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) ||(spo2 < 80) || (respRate < 30 || respRate > 60) {
-    return false;
-  } 
-  return true;
+
+  if(bpmcheck(bpm) ||(spo2check(spo2)) || (respRatecheck(respRatecheck)
+           return false;
+ return true;
 }
