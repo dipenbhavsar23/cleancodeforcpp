70aadddfb59483d9cd859c47f72572d536d6d23e
dipenbhavsar23 <dipenbhavsar.23@gmail.com>
2020-07-20 15:50:44 +0530

    Error solve

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -4,12 +4,12 @@ bool bpmcheck(float bpm){
 bool spo2check(float spo2){
 return (spo2 < 80);
 }
-bool respRatecheck(float respRatecheck){
+bool respRatecheck(float respRate){
 return (respRate < 30 || respRate > 60);
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
 
-  if(bpmcheck(bpm) ||(spo2check(spo2)) || (respRatecheck(respRatecheck)
+  if(bpmcheck(bpm) ||(spo2check(spo2)) || (respRatecheck(respRatecheck)))
            return false;
  return true;
 }
