1ed193ff29ed548ff30ede83571150b51af7d1bc
dipenbhavsar23 <dipenbhavsar.23@gmail.com>
2020-07-20 15:36:23 +0530

    Reducing CCN
    Check all the condition with one decision as it returns same

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,11 +1,7 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  if(bpm < 70 || bpm > 150) {
+  if(bpm < 70 || bpm > 150) ||(spo2 < 80) || (respRate < 30 || respRate > 60) {
     return false;
-  } else if(spo2 < 80) {
-    return false;
-  } else if(respRate < 30 || respRate > 60) {
-    return false;
-  }
+  } 
   return true;
 }
