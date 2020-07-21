9b80e71942fa3707b55978a638209649795011ab
dipenbhavsar23 <dipenbhavsar.23@gmail.com>
2020-07-21 16:06:46 +0530

    Open Closed Solution 1

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,12 +1,9 @@
-bool bpmcheck(float bpm){ 
-  return(bpm < 70 || bpm > 150);
+bool checklimits(float data,float upper,float lower){ 
+  return(data < lower || data > upper);
 }
-bool spo2check(float spo2){
-return (spo2 < 80);
-}
-bool respRatecheck(float respRate){
-return (respRate < 30 || respRate > 60);
+bool spo2check(float spo2,upper){
+return (data < upper);
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-  return(bpmcheck(bpm) ||(spo2check(spo2)) || (respRatecheck(respRate)));
+    return(checklimits(bpm,C_BPM_LOWLIMIT_F,C_BPM_UPPPER_LIMIT_F) ||(spo2check(spo2,C_SPO2_UPPER_F)) || (respRatecheck(respRate,C_RESP_LOWLIMIT_F,C_RESP_UPPPER_LIMIT_F)));
 }
