41bcafb8b8d84e6818194ce3c9d368aeca167d57
dipenbhavsar23 <dipenbhavsar.23@gmail.com>
2020-07-21 16:06:54 +0530

    open close solution 1

--- paramchecker.h
+++ paramchecker.h
@@ -1,2 +1,7 @@
 
 bool vitalsAreOk(float bpm, float spo2, float respRate);
+#define C_BPM_LOWLIMIT_F      70
+#define C_BPM_UPPPER_LIMIT_F  150
+#define C_RESP_LOWLIMIT_F     30
+#define C_RESP_UPPPER_LIMIT_F 60
+#define C_SPO2_UPPER_F        80
