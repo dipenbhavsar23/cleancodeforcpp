1234768db1be441f26215f8fc6d15478e62ef176
dipenbhavsar23 <dipenbhavsar.23@gmail.com>
2020-07-22 09:35:42 +0530

    Update paramchecker.cpp

--- paramchecker.cpp
+++ paramchecker.cpp
@@ -1,10 +1,7 @@
 #include "paramchecker.h"
 bool checklimits(float data,float upper,float lower){ 
-  return(data < lower || data > upper);
-}
-bool checklower(float data,float upper){
-return (data < upper);
+  return((data < lower) || (data > upper));
 }
 bool vitalsAreOk(float bpm, float spo2, float respRate) {
-    return(checklimits(bpm,C_BPM_LOWLIMIT_F,C_BPM_UPPPER_LIMIT_F) ||(checklower(spo2,C_SPO2_UPPER_F)) || (checklimits(respRate,C_RESP_LOWLIMIT_F,C_RESP_UPPPER_LIMIT_F)));
+    return(checklimits(bpm,C_BPM_LOWLIMIT_F,C_BPM_UPPPER_LIMIT_F) ||(checklimits(spo2,0,C_SPO2_UPPER_F)) || (checklimits(respRate,C_RESP_LOWLIMIT_F,C_RESP_UPPPER_LIMIT_F)));
 }
